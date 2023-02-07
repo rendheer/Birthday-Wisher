8d90300f51e913c04e458ef08c9a9e7ba02b6085
rendheer <rendheer_joshy@hotmail.com>
2023-02-07 13:22:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import random
 import smtplib
 
 MY_EMAIL = "rendheer_joshy@yahoo.com"
-MY_PASSWORD = "toxjwadamcckymbh"
+MY_PASSWORD = ""
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
